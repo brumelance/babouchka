5d0d4d35cc5141a85b3d42910493a91675f228d7
Anthony Liard <anthonyliard1993@gmail.com>
2020-01-14 20:30:13 -0500

    removing gender in account creation

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,22 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface Sexe {
-  value: string;
-  viewValue: string;
-}
-
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  sexes: Sexe[] = [
-    {value: '1', viewValue: 'Homme'},
-    {value: '2', viewValue: 'Femme'},
-    {value: '3', viewValue: 'Autres'},
-    {value: '4', viewValue: 'Ne pas préciser'}
-  ];
 
   constructor() { }
 
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
-import { 
-  MatFormFieldModule,
-  MatInputModule,
-  MatButtonModule,
-  MatIconModule,
-  MatRadioModule,
-  MatSidenavModule,
-  MatToolbarModule,
-  MatListModule,
-  MatSelectModule
- } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
